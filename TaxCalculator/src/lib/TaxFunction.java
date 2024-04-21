2c0eb01e26602cf66e6f0a46950a42eee092789c
Faris Siddiq <farissiddiqrp@gmail.com>
2024-04-21 13:13:43 +0700

    Menambah komentar di TaxFunction
    Menambahkan komentar di setiap method yang ada di TaxFunction

--- TaxCalculator/src/lib/TaxFunction.java
+++ TaxCalculator/src/lib/TaxFunction.java
@@ -14,38 +14,45 @@ public class TaxFunction {
 	 * 
 	 */
 	
-	
-	 public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
+	// Method untuk menghitung jumlah pajak penghasilan pegawai yang harus dibayarkan setahun
+	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
         int tax = 0;
 
+		// Memastikan jika jumlah bulan bekerja melebihi 12 bulan per tahun
         if (numberOfMonthWorking > 12) {
             System.err.println("More than 12 month working per year");
         }
 
-        numberOfChildren = Math.min(numberOfChildren, 3); 
+        numberOfChildren = Math.min(numberOfChildren, 3); // Batasi jumlah anak maksimal menjadi 3
 
+		// Hitung penghasilan yang dikenakan pajak dan pemotongan pajak
         int taxableIncome = calculateTaxableIncome(monthlySalary, otherMonthlyIncome, numberOfMonthWorking, isMarried, numberOfChildren);
         int taxDeduction = calculateTaxDeduction(isMarried, numberOfChildren);
 
         tax = (int) Math.round(0.05 * (taxableIncome - deductible - taxDeduction));
 
-        return Math.max(tax, 0); 
+        return Math.max(tax, 0); // Memastikan pajak tidak kurang dari 0
     }
 
+	// Method untuk menghitung penghasilan yang dikenakan pajak
     private static int calculateTaxableIncome(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, boolean isMarried, int numberOfChildren) {
+		// Hitung penghasilan dasar dan pemotongan pajak
         int basicIncome = (monthlySalary + otherMonthlyIncome) * numberOfMonthWorking;
         int taxExemption = calculateTaxExemption(isMarried, numberOfChildren);
 
         return basicIncome - taxExemption;
     }
 
+	// Method untuk menghitung pemotongan pajak
     private static int calculateTaxExemption(boolean isMarried, int numberOfChildren) {
+		// Tentukan pemotongan dasar dan pemotongan berdasarkan jumlah anak
         int baseExemption = isMarried ? 54000000 + 4500000 : 54000000;
         int childrenExemption = numberOfChildren * 1500000;
 
         return baseExemption + childrenExemption;
     }
 
+	// Method untuk menghitung pemotongan pajak
     private static int calculateTaxDeduction(boolean isMarried, int numberOfChildren) {
         return calculateTaxExemption(isMarried, numberOfChildren);
     }
