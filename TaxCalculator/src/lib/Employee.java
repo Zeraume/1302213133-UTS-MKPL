1197b84cb6d801c2c93d99b5fb89d33649d857a9
Faris Siddiq <farissiddiqrp@gmail.com>
2024-04-21 13:07:24 +0700

    Menambahkan komentar di Employee
    Menambahkan komentar di setiap method yang berada di Employee

--- TaxCalculator/src/lib/Employee.java
+++ TaxCalculator/src/lib/Employee.java
@@ -12,6 +12,7 @@ public class Employee {
     private int annualDeductible;
     private FamilyData familyData;
 
+    // Konstruktor untuk inisialisasi objek Employee
     public Employee(EmployeeData employeeData, Grade grade) {
         this.employeeData = employeeData;
         this.grade = grade;
@@ -23,27 +24,33 @@ public class Employee {
 	 * Jika pegawai adalah warga negara asing gaji bulanan diperbesar sebanyak 50%
 	 */
 	
+	// Method untuk mengatur gaji bulanan pegawai
 	 public void setMonthlySalary() {
         int baseSalary = getBaseSalaryByGrade(grade);
         monthlySalary = employeeData.isForeigner() ? (int) (baseSalary * 1.5) : baseSalary;
     }
 	
+	// Method untuk mengatur potongan tahunan
 	public void setAnnualDeductible(int deductible) {
         this.annualDeductible = deductible;
     }
 
+    // Method untuk mengatur penghasilan bulanan lainnya
     public void setOtherMonthlyIncome(int income) {
         this.otherMonthlyIncome = income;
     }
 
+    // Method untuk mengatur data pasangan pegawai
     public void setSpouse(String spouseName, String spouseIdNumber) {
         this.familyData.setSpouse(spouseName, spouseIdNumber);
     }
 
+    // Method untuk menambahkan data anak pegawai
     public void addChild(String childName, String childIdNumber) {
         this.familyData.addChild(childName, childIdNumber);
     }
 	
+	// Method untuk menghitung pajak tahunan
 	public int getAnnualIncomeTax() {
         int monthsWorked = calculateMonthsWorked();
         boolean isSingle = familyData.isSingle();
@@ -52,6 +59,7 @@ public class Employee {
         return TaxFunction.calculateTax(monthlySalary, otherMonthlyIncome, monthsWorked, annualDeductible, isSingle, numChildren);
     }
 
+	// Method untuk menghitung jumlah bulan kerja
 	private int calculateMonthsWorked() {
         int currentYear = Year.now().getValue();
         int monthsWorked;
@@ -64,6 +72,7 @@ public class Employee {
         return monthsWorked;
     }
 
+	// Method untuk mendapatkan gaji dasar berdasarkan grade
 	private int getBaseSalaryByGrade(Grade grade) {
         switch (grade) {
             case GRADE_1:
